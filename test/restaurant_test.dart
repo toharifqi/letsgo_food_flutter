3c656d7d70344d240e9c3901f471e9af31ffb1fd
Rifqi Naufal Tohari <rifqinaufaltohari@gmail.com>
2023-05-14 21:08:05 +0700

    regroup some tests

--- test/restaurant_test.dart
+++ test/restaurant_test.dart
@@ -2,15 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:letsgo_food/data/model/restaurant_model.dart';
 
 void main() {
-  test("fromRawJson, when given with string json, should return Restaurant with matched values", () {
-    const id = "rqdv5juczeskfw1e867";
-    const name = "Melting";
-    const description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ...";
-    const pictureId = "14";
-    const city = "Medan";
-    const rating = 4.2;
+  group("Unit Test For restaurant.dart", () {
 
-    const rawJson = """
+    test("fromRawJson, when given with string json, should return Restaurant with matched values", () {
+      const id = "rqdv5juczeskfw1e867";
+      const name = "Melting";
+      const description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ...";
+      const pictureId = "14";
+      const city = "Medan";
+      const rating = 4.2;
+
+      const rawJson = """
       {
           "id": "$id",
           "name": "$name",
@@ -21,67 +23,69 @@ void main() {
       }
     """;
 
-    final result = Restaurant.fromRawJson(rawJson);
+      final result = Restaurant.fromRawJson(rawJson);
 
-    expect(result.id, id);
-    expect(result.name, name);
-    expect(result.description, description);
-    expect(result.pictureId, pictureId);
-    expect(result.city, city);
-    expect(result.rating, rating);
-  });
+      expect(result.id, id);
+      expect(result.name, name);
+      expect(result.description, description);
+      expect(result.pictureId, pictureId);
+      expect(result.city, city);
+      expect(result.rating, rating);
+    });
 
-  test("fromJson, when given with json in map, should return Restaurant with matched values", () {
-    const id = "rqdv5juczeskfw1e867";
-    const name = "Melting";
-    const description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ...";
-    const pictureId = "14";
-    const city = "Medan";
-    const rating = 4.2;
+    test("fromJson, when given with json in map, should return Restaurant with matched values", () {
+      const id = "rqdv5juczeskfw1e867";
+      const name = "Melting";
+      const description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. ...";
+      const pictureId = "14";
+      const city = "Medan";
+      const rating = 4.2;
 
-    final json = {
-      "id": id,
-      "name": name,
-      "description": description,
-      "city": city,
-      "pictureId": pictureId,
-      "rating": rating,
-    };
+      final json = {
+        "id": id,
+        "name": name,
+        "description": description,
+        "city": city,
+        "pictureId": pictureId,
+        "rating": rating,
+      };
 
-    final result = Restaurant.fromJson(json);
+      final result = Restaurant.fromJson(json);
 
-    expect(result.id, id);
-    expect(result.name, name);
-    expect(result.description, description);
-    expect(result.pictureId, pictureId);
-    expect(result.city, city);
-    expect(result.rating, rating);
-  });
+      expect(result.id, id);
+      expect(result.name, name);
+      expect(result.description, description);
+      expect(result.pictureId, pictureId);
+      expect(result.city, city);
+      expect(result.rating, rating);
+    });
 
-  test("toJson, when given with Restaurant, should return json with matched values", () {
-    const id = "rqdv5juczeskfw1e867";
-    const name = "Melting";
-    const pictureId = "14";
-    const city = "Medan";
-    const rating = 4.2;
+    test("toJson, when given with Restaurant, should return json with matched values", () {
+      const id = "rqdv5juczeskfw1e867";
+      const name = "Melting";
+      const pictureId = "14";
+      const city = "Medan";
+      const rating = 4.2;
 
-    final restaurant = Restaurant(
-        id: id,
-        name: name,
-        description: null,
-        city: city,
-        pictureId: pictureId,
-        categories: List.empty(),
-        rating: rating,
-        customerReviews: List.empty()
-    );
+      final restaurant = Restaurant(
+          id: id,
+          name: name,
+          description: null,
+          city: city,
+          pictureId: pictureId,
+          categories: List.empty(),
+          rating: rating,
+          customerReviews: List.empty()
+      );
 
-    final result = restaurant.toJson();
+      final result = restaurant.toJson();
 
-    expect(result["id"], id);
-    expect(result["name"], name);
-    expect(result["pictureId"], pictureId);
-    expect(result["city"], city);
-    expect(result["rating"], rating);
+      expect(result["id"], id);
+      expect(result["name"], name);
+      expect(result["pictureId"], pictureId);
+      expect(result["city"], city);
+      expect(result["rating"], rating);
+    });
+
   });
 }
